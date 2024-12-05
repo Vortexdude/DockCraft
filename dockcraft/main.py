7eef64681e6279a6122a17cac1b63f564453f342
vortexdude <nitinnamdeo456@gmail.com>
2024-12-05 15:49:53 +0530

    :palm_tree: adding requirements.txt

--- dockcraft/main.py
+++ dockcraft/main.py
@@ -4,11 +4,11 @@ from dockcraft.client import DockerClient
 if __name__ == "__main__":
     api = DockerClient.from_env()
 
-    # container = api.containers.create("python", name="anything", command="sleep infinity")
-    # containers = api.containers.list(all_containers=True)
-    # for cont in containers:
-    #     data = cont.rename("from-new-model")
-    #     print(data)
+    container = api.containers.create("python", name="anything", command="sleep infinity")
+    containers = api.containers.list(all_containers=True)
+    for cont in containers:
+        data = cont.rename("from-new-model")
+        print(data)
 
     # api.containers.get(containers[0].short_id)
     # container = api.containers.create("python", name="anything", command="sleep infinity")
